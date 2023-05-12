b716743349dae1536955d948e702755a2e9e8b91
amuratakamitamu <99322578+amuratakamitamu@users.noreply.github.com>
2023-05-12 16:32:12 +0900

    complete exercize6.c

--- grade3/exercoze6.c
+++ grade3/exercoze6.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#define MIN(num1, num2, num3) (num1 < num2 ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3));
+
+int num1, num2, num3, ans;
 
 int main(void)
 {
+    printf("任意の整数を入力してください。(1)\n");
+    scanf("%d", &num1);
+    printf("任意の整数を入力してください。(2)\n");
+    scanf("%d", &num2);
+    printf("任意の整数を入力してください。(3)\n");
+    scanf("%d", &num3);
+
+    ans = MIN(num1, num2, num3);
+    printf("入力された整数のうち、最小値は%dです。\n", ans);
+
     return 0;
 }
